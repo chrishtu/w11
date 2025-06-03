478957e82feddcbf8451f33e37f1099ab1c3eeea
MinhTu <ductrinh.anhtuyb@gmail.com>
2025-06-04 06:22:30 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('w11/sw.js', { scope: '/w11/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
